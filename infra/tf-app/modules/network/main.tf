da0533488cd546e85dc54565c004c640309f7710
kare0041 <kare0041@algonquinlive.com>
2025-08-07 18:59:57 -0400

    rearranging the file structrue for the network terraform module

--- infra/tf-app/modules/network/main.tf
+++ infra/tf-app/modules/network/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "network_rg" {
-  name     = "cst8918-final-project-group-3"
+  name     = var.network_rg_name
   location = var.location
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
-  name                = "grp3-vnet"
+  name                = var.main_vnet_name
   address_space       = ["10.0.0.0/14"]
   location            = var.location
   resource_group_name = azurerm_resource_group.network_rg.name
