bd314d29499b9e198048ef1dfd716be0542dec6e
kare0041 <kare0041@algonquinlive.com>
2025-08-08 20:33:21 -0400

    adding the remix weather app folder

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -13,6 +13,17 @@ module "aks" {
   resource_group_name = module.network.resource_group_name
   vnet_subnet_id_test = module.network.test_subnet_id
   vnet_subnet_id_prod = module.network.prod_subnet_id
-  test_aks_name       = "grp3-aks-test"
-  prod_aks_name       = "grp3-aks-prod"
+  test_aks_name       = "grp3akstest"
+  prod_aks_name       = "grp3aksprod"
+}
+
+module "acr" {
+  source               = "./modules/acr"
+  location             = var.location
+  resource_group_name  = module.network.resource_group_name
+  acr_name             = "grp3weatheracr"
+  redis_test_name      = "grp3redistest"
+  redis_prod_name      = "grp3redisprod"
+  test_aks_identity_id = module.aks.test_aks_identity_id
+  prod_aks_identity_id = module.aks.prod_aks_identity_id
 }
--- infra/tf-app/modules/acr/main.tf
+++ infra/tf-app/modules/acr/main.tf
@@ -1,57 +1,57 @@
-
-# ---------- ACR ----------
 resource "azurerm_container_registry" "weather_app_acr" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = var.acr_sku
-  admin_enabled       = var.acr_admin_enabled
+  sku                 = "Basic"
+  admin_enabled       = true
 }
 
-# ---------- Redis (TEST) ----------
 resource "azurerm_redis_cache" "redis_test" {
-  name                = var.redis_test_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  capacity            = var.redis_capacity
-  family              = var.redis_family
-  sku_name            = var.redis_sku
-
-  non_ssl_port_enabled = var.redis_non_ssl_port_enabled
+  name                 = var.redis_test_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  capacity             = 1
+  family               = "C"
+  sku_name             = "Basic"
+  non_ssl_port_enabled = true
 }
 
-# ---------- Redis (PROD) ----------
 resource "azurerm_redis_cache" "redis_prod" {
-  name                = var.redis_prod_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  capacity            = var.redis_capacity
-  family              = var.redis_family
-  sku_name            = var.redis_sku
-
-  non_ssl_port_enabled = var.redis_non_ssl_port_enabled
+  name                 = var.redis_prod_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  capacity             = 1
+  family               = "C"
+  sku_name             = "Basic"
+  non_ssl_port_enabled = true
 }
 
-# ---------- ACR Pull for AKS TEST ----------
+# Grant ACR Pull access to AKS Test cluster
 resource "azurerm_role_assignment" "aks_test_acr_pull" {
-  scope                = azurerm_container_registry.weather_app_acr.id
+  principal_id         = var.test_aks_identity_id
   role_definition_name = "AcrPull"
-  principal_id         = var.aks_test_kubelet_object_id
+  scope                = azurerm_container_registry.weather_app_acr.id
 
   lifecycle {
-    ignore_changes = [principal_id, role_definition_name, scope]
+    ignore_changes = [
+      principal_id,
+      role_definition_name,
+      scope
+    ]
   }
 }
 
-# ---------- ACR Pull for AKS PROD ----------
+# Grant ACR Pull access to AKS Prod cluster
 resource "azurerm_role_assignment" "aks_prod_acr_pull" {
-  scope                = azurerm_container_registry.weather_app_acr.id
+  principal_id         = var.prod_aks_identity_id
   role_definition_name = "AcrPull"
-  principal_id         = var.aks_prod_kubelet_object_id
+  scope                = azurerm_container_registry.weather_app_acr.id
 
   lifecycle {
-    ignore_changes = [principal_id, role_definition_name, scope]
+    ignore_changes = [
+      principal_id,
+      role_definition_name,
+      scope
+    ]
   }
 }
